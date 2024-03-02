b83b0ee5c87779490816ac62c9619638c309569b
changraekang <storkcr@gmail.com>
2024-03-03 01:02:00 +0900

    점수창

--- src/pages/AnswerSheet.js
+++ src/pages/AnswerSheet.js
@@ -10,9 +10,9 @@ import {
 import styled from "styled-components";
 import axios from "axios";
 import Loading from "../components/Loading";
-import Button from "react-bootstrap/Button";
 import { useNavigate } from "react-router-dom";
 import config from "../config";
+import { Table, Form, Input, Button, message } from "antd";
 
 const AnswerSheet = () => {
   const [quizidx, setQuizidx] = useRecoilState(QuizQuestion);
@@ -90,130 +90,58 @@ const AnswerSheet = () => {
     navigate(`/ranking?userId=${rankuser.userId}&username=${rankuser.user}`);
   };
   return (
-    <Flex>
-      <UserAnswerSheet>
-        <Header>{rankuser.user}의 답</Header>
-        <Tabs>
-          <No> No</No>
-          <QuizAnswer2> 문제</QuizAnswer2>
-          <UserAnswer> 답안</UserAnswer>
-        </Tabs>
-        {loading ? (
-          <Loading />
-        ) : (
-          <Quizzes>
-            <FlexColumn>
-              {quiztitlelength.map((row, idx) => (
-                <TradeOdd
-                  style={
-                    idx % 2 != 0
-                      ? { backgroundColor: "white" }
-                      : { backgroundColor: "rgb(232, 246, 255)" }
-                  }
-                >
-                  <No>{idx + 1}</No>
-                  <QuizAnswer2>{quiztitlelength.answer}</QuizAnswer2>
-                </TradeOdd>
-              ))}
-            </FlexColumn>
-            <FlexColumn2>
-              {quizans.map((row, idx) => (
-                <TradeOdd
-                  style={
-                    idx % 2 != 0
-                      ? { backgroundColor: "white" }
-                      : { backgroundColor: "rgb(232, 246, 255)" }
-                  }
-                >
-                  {row}
-                </TradeOdd>
-              ))}
-            </FlexColumn2>
-          </Quizzes>
-        )}
-        <Button onClick={submitScore}>결과제출</Button>
-      </UserAnswerSheet>
-    </Flex>
+    <Container>
+      <Header>Quiz Answer Sheet</Header>
+      <Subtitle>Fill in your answers and scores below</Subtitle>
+      {[1, 2, 3, 4, 5, 6].map((number) => (
+        <QuizItem key={number}>
+          <Question>Quiz {number}</Question>
+          <AnswerButton>Answer</AnswerButton>
+        </QuizItem>
+      ))}
+    </Container>
   );
 };
 
 export default AnswerSheet;
-const UserAnswerSheet = styled.div`
-  height: 100vh;
-  width: 70%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-`;
-const Tabs = styled.div`
-  display: flex;
-  font-size: 12px;
-  width: 100%;
-  padding-left: 10px;
-  padding-right: 10px;
-  flex: 0 0 30px;
-  align-items: center;
-  border-bottom: 1px solid gray;
-`;
-
-const Quizzes = styled.div`
-  display: flex;
-  width: 100%;
-  flex: 0 0 30px;
-`;
-
-const TradeOdd = styled.div`
-  display: flex;
-  flex-direction: row;
-  font-size: 14px;
-  width: 100%;
-  height: 23px;
-  background-color: rgb(232, 246, 255);
-`;
-const TradeEven = styled.div`
-  display: flex;
-  font-size: 14px;
-  height: 23px;
-`;
-
-const No = styled.div`
-  width: 100%;
-  text-align: center;
-`;
-
-const UserAnswer = styled.div`
-  width: 100%;
-  text-align: center;
-`;
-
-const QuizAnswer2 = styled.div`
-  width: 100%;
-  text-align: center;
-`;
-
-const Rise = styled.div`
-  color: #d60000;
-`;
-const Flex = styled.div`
-  display: flex;
-  width: 100%;
-  justify-content: center;
-`;
-const FlexColumn = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 66%;
-`;
-const FlexColumn2 = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 33%;
-`;
-const Header = styled.div`
-  font-size: 35pt;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+const Container = styled.div`
   font-family: "DoHyeon-Regular";
+`;
+
+const Header = styled.div`
+  font-size: 24px;
+  margin-bottom: 20px;
+  text-align: center;
+  font-family: "DoHyeon-Regular";
+`;
+
+const Subtitle = styled.div`
+  font-size: 18px;
+  color: #666;
+  margin-bottom: 30px;
+  text-align: center;
+  font-family: "DoHyeon-Regular";
+`;
+
+const QuizItem = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 10px;
+  padding: 10px;
+  border-bottom: 1px solid #ccc;
+  font-family: "DoHyeon-Regular";
+`;
+
+const Question = styled.div`
+  font-size: 18px;
+  font-family: "DoHyeon-Regular";
+`;
+
+const AnswerButton = styled.button`
+  padding: 10px 20px;
+  background-color: black;
+  color: white;
+  border: none;
+  cursor: pointer;
 `;
--- src/pages/Quiz.js
+++ src/pages/Quiz.js
@@ -30,8 +30,9 @@ const Quiz = () => {
   const [hint1, setHint1] = useState(true);
   const [hint2, setHint2] = useState(false);
   const [hint3, setHint3] = useState(false);
-  const hintString = "O".repeat(quiztitlelength[quizNum - 1].length);
-  console.log(quiztitlelength, ":: quiztitlelength");
+  const hintString = "O".repeat(
+    quiztitlelength[quizNum - 1].length - Answer.length
+  );
   useEffect(() => {
     console.log(quizHintUsed, ":: QuizHintUsed");
   }, [quizidx.length, quizHintUsed, setQuizHintUsed]);
@@ -89,26 +90,66 @@ const Quiz = () => {
         }}
       >
         <Title>{quizNum} Quiz</Title>
-        <Hint>정답: {quiztitlelength[quizNum - 1].length}글자</Hint>
+        <Hint>Points: </Hint>
       </div>
       <ButtonWrapper>
         <Button onClick={() => onClickHint(1)}>
-        <img src={trail} alt="trail" width={36} height={36} />{" "}
-          <div></div>
+          <img src={trail} alt="trail" width={36} height={36} />{" "}
+          <Hint>Hint1 - 명대사 & OST</Hint>
         </Button>
-        <Button onClick={() => onClickHint(2)}><img src={film} alt="film" width={36} height={36} />{" "}</Button>
-        <Button onClick={() => onClickHint(3)}><img src={poster} alt="poster" width={36} height={36} />{" "}</Button>
+        <Button onClick={() => onClickHint(2)}>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+              width: "100%",
+            }}
+          >
+            <img src={film} alt="film" width={36} height={36} />
+            <Hint>-1</Hint>
+          </div>
+          <Hint>Hint2 - 영화 속 장면</Hint>
+        </Button>
+        <Button onClick={() => onClickHint(3)}>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+              width: "100%",
+            }}
+          >
+            <img src={poster} alt="poster" width={36} height={36} />{" "}
+            <Hint>-3</Hint>
+          </div>
+          <Hint>Hint3 - 영화 포스터</Hint>
+        </Button>
       </ButtonWrapper>
 
       <InputContainer>
         <InputLabel>정답</InputLabel>
-        <Input
-          type="input"
-          placeholder="정답을 입력해주세요"
-          onChange={(e) => setAnswer(e.target.value.trim())}
-          value={Answer}
-          onKeyPress={(e) => e.key === "Enter" && onClickSubmit()}
-        />
+        <div>
+          <AnswerSheet>
+            {Answer}
+            {hintString}
+          </AnswerSheet>
+          <Input
+            type="input"
+            placeholder="정답을 입력해주세요"
+            onChange={(e) => {
+              const newValue = e.target.value.trim();
+              // 입력 값의 길이가 quiztitlelength[quizNum - 1].length 이하인지 확인
+              if (newValue.length <= quiztitlelength[quizNum - 1].length) {
+                setAnswer(newValue); // 조건을 만족하면 상태 업데이트
+              }
+            }}
+            value={Answer}
+            onKeyPress={(e) => e.key === "Enter" && onClickSubmit()}
+          />
+        </div>
         <Button onClick={onClickSubmit}>제출하기</Button>
       </InputContainer>
       <HintWrapper>{renderHintComponent()}</HintWrapper>
@@ -128,8 +169,9 @@ const Wrapper = styled.div`
 `;
 const InputContainer = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   margin-top: 20px;
+  align-items: center;
 `;
 const QuizContainer = styled.div`
   display: flex;
@@ -142,14 +184,15 @@ const InputLabel = styled.label`
   margin: 0px;
   font-weight: bold;
   font-family: "DoHyeon-Regular";
+  padding-right: 10px;
 `;
-
 const Input = styled.input`
   margin-top: 10px;
   font-size: 12px;
   color: #033e93;
   height: 30px;
-  border: 1px solid rgb(200, 200, 200);
+  border: 0; /* 나머지 테두리를 제거합니다 */
+  border-bottom: 1px solid #033e93; /* 아래쪽 테두리만 추가 */
   padding-left: 10px;
   padding-right: 10px;
 `;
@@ -176,6 +219,7 @@ const Button = styled.div`
   align-items: center;
   -webkit-box-pack: center;
   justify-content: center;
+  flex-direction: column;
   padding: 0.3rem 0.875rem;
   font-size: 0.875rem;
   cursor: pointer;
@@ -190,6 +234,13 @@ const Hint = styled.div`
   font-size: 10pt;
   display: flex;
   justify-content: flex-end;
+  align-items: center;
+  font-family: "DoHyeon-Regular";
+`;
+const AnswerSheet = styled.div`
+  font-size: 10pt;
+  display: flex;
+  justify-content: flex-start;
   align-items: center;
   font-family: "DoHyeon-Regular";
 `;
