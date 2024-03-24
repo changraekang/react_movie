eb9cd306500c562034c749839200c1c1aa18f983
changraekang <storkcr@gmail.com>
2024-03-24 13:13:05 +0900

    점수창 기입 유저네임 추가

--- src/pages/AnswerSheet.js
+++ src/pages/AnswerSheet.js
@@ -98,9 +98,9 @@ const AnswerSheet = () => {
   // useEffect to show popup message based on number of correct answers
   useEffect(() => {
     if (correctAnswers >= 6) {
-      setAnswerScore("씨네필이네요!");
+      setAnswerScore(username + "님은 씨네필이네요!🎉🎉");
     } else if (correctAnswers >= 3) {
-      setAnswerScore("영화 매니아시네요!");
+      setAnswerScore(username + "님은 영화 매니아시네요!👍👍");
     } else {
       setAnswerScore(`${correctAnswers}개 맞추셨습니다.`);
     }
